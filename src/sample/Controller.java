1f2702e0f57242b9024789569374812a12ab798b
HarunNB <cemalovicharun@gmail.com>
2019-04-01 16:24:21 +0200

    Napravio klasu artikal

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,10 @@
 package sample;
 
 public class Controller {
+    public TextArea sourceField;
+    public TextArea destinationField;
+
+    public void addToReceipt(Actionevent actionevent) {
+        String source = sourceField.get
+    }
 }
